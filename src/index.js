e709d91e8385847b9f24fdeaac517f2b3027b450
Aditya Nur Rohman <xditx32@gmail.com>
2021-04-24 12:44:51 +0700

    first push

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="everyday06.au.auth0.com"
+    clientId="3lZK312sgFlXnWTsPjOMIbWvRrtWHsyZ"
+    redirectUri="http://localhost:3000"
+    scope="read:current_user update:current_user_metadata"
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
